46d9f3b0dc1734f2a7a407d1e292de843292eccd
stela88 <stela88@users.noreply.github.com>
2021-05-06 13:05:23 +0200

    dodano što se obrisalo

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// Nadopuniti u datoteku "src/App.vue" iznad retka: this.cards = []
+// ...
+
+// fetch("http://localhost:3330/posts")
+//  .then(r => {
+//  return r.json()
+//  })
+//  then(data => {
+//  console.log("Podaci s backenda", data)
+//  })
+// stari kod, ne prepisivati, čisto za referencu
+
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+
+
+
---
+++ src/views/Pocetna.vue
@@ -0,0 +1,43 @@
+<template>
+<body>
+     <div class="pocetna">
+         <h1>Ukoliko ste ljubitelj <br>kvalitetnih vina, na pravom <br>ste mjestu!</h1>
+     <img alt="Logo vina" src="../assets/logovina.png"> <br>
+     <button>Pogledaj ponudu</button>  
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  name: 'Pocetna',
+}
+</script>
+
+<style>
+body {
+  background-color: #1d2327;
+}
+h1{
+    color: aliceblue;
+    font-family:  "Times New Roman", Times, serif;;
+}
+img{
+    width:300px;
+} 
+button{
+  background-color: white;
+  border-radius: 20px;
+  border-color: white;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  font-size: 16px;
+  color: #b71c1c;
+  
+}
+
+
+
+
+</style>

5bb26e53da21cab52232e668402c5dc18aa24faa
stela88 <49338331+stela88@users.noreply.github.com>
2021-08-03 16:01:28 +0200

    Homecard

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="homecard">
     <router-link :to="info.page">
-    <img alt="homecard" :src="info.img" class="vino"> </router-link> 
-    <br><br>
-     <h4><p>{{ info.description }}</p></h4>
-     </div>
-
+      <img alt="homecard" :src="info.img" class="vino" />
+    </router-link>
+    <br /><br />
+    <h4>
+      <p>{{ info.description }}</p>
+    </h4>
+  </div>
 </template>
+
 
 <script>
 export default {

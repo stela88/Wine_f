6a84c733997b4645e8d0a8ec9753150aeb3725dd
stela88 <49338331+stela88@users.noreply.github.com>
2021-08-11 23:59:45 +0200

    Cabernet Sauvignon

--- src/views/Cabernet_Sauvignon.vue
+++ src/views/Cabernet_Sauvignon.vue
@@ -17,32 +17,41 @@ import WineCard from "../components/WineCard.vue";
 
 let winecard = [];
 
-winecard = [
-  {
-    logo: require("@/assets/logo2.png"),
-    ime: "CABERNET SAUVIGNON",
-    slika: require("@/assets/cabernet_sauvignon_crna.jpg"),
-    boja: "bijelo",
-    temp: "16-18 C",
-    cijena: "40 Kn",
-    opis: "Duboke, rubin crvene boje s granatnim odsjajem. Bistro i gusto, zagasitih refleksija. slatkastih nota, zrelog crnog bobičastog voća, poput marmelade od crnog ribiza. Začinskih nota crnog papra, timjana i rogača. Na nepcu je mekano, svježe i trajno. Blagih zrelih tanina trajnog okusa.",
-    posluzi: "Gulaš od vepra s njokima ili juneću potrbušinu na žaru i pačja prsa s aronijom.",
-    jelo1: require("@/assets/jelo1_cabernet_sauvignon.jpg"),
-    jelo2: require("@/assets/jelo2_cabernet_sauvignon.jpg"),
-    jelo3: require("@/assets/jelo3_cabernet_sauvignon.jpg"),
-    link1: "https://finirecepti.net.hr/priprema/gulas-od-vepra-na-istarski-nacin/",
-    link2: "https://gastro.24sata.hr/recepti/flank-steak-na-grillu-nepravedno-zanemaren-komad-junetine-23559",
-    link3: "https://www.coolinarika.com/recept/pacja-prsa-s-aronijom-d748f712-6182-11eb-b558-0242ac120046",
-  },
-]
-
 export default {
-  name: "Cabernet Sauvignon",
+  name: "Cabernet_Sauvignon",
   data(){
     return{
       winecard,
     }
   },
+  async mounted() {
+    fetch("http://localhost:3330/cabernet_sauvignon")
+      .then((r) => {
+        return r.json();
+      })
+      .then((data) => {
+        console.log("Podaci s backenda, cabernet sauvignon", data);
+
+        this.winecard = data.map((element) => {
+          return {
+            logo: element.logo,
+            ime: element.name,
+            slika: element.img,
+            boja: element.color,
+            temp: element.temp,
+            cijena: element.price,
+            opis: element.about,
+            posluzi: element.serve,
+            jelo1: element.meal1,
+            jelo2: element.meal2,
+            jelo3: element.meal3,
+            link1: element.link1,
+            link2: element.link2,
+            link3: element.link3
+          };
+        });
+      });
+   },
   components: {
     WineCard
   }

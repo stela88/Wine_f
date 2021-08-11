530efa84f6a04f2f06b710f1369a827a10296ca5
stela88 <49338331+stela88@users.noreply.github.com>
2021-08-11 23:10:07 +0200

    Brut dodan

--- src/views/Brut.vue
+++ src/views/Brut.vue
@@ -17,24 +17,6 @@ import WineCard from "../components/WineCard.vue";
 
 let winecard = [];
 
-winecard = [
-  {
-    logo: require("@/assets/logo2.png"),
-    ime: "BRUT",
-    slika: require("@/assets/brut_crna.jpg"),
-    boja: "bijelo",
-    temp: "5-7 C",
-    cijena: "50 Kn",
-    opis: "Vino je sjajne, slamnato žute boje s blagim zelenkastim odsjajem, vrlo živahnih refleksija. Mjehurići su sitni, brojni i postojani, kako u čaši tako i na nepcu. Svježeg je aromatskog profila, po poljskom cvijeću, bijeloj vinogradarskoj breskvi i limeti. Na nepcu je izrazito svježe i mekano. Sirupastog i trajnog okusa.",
-    posluzi: "Finger food i hladna riblja predjela: sirovi kvarnerski škampi, sirove kamenice s vinaigrettom od zelene jabuke.",
-    jelo1: require("@/assets/jelo1_brut.jpg"),
-    jelo2: require("@/assets/jelo2_brut.jpg"),
-    jelo3: require("@/assets/jelo3_brut.jpg"),
-    link1: "https://www.coolinarika.com/inspiracija/finger-food-c6f308e6-610d-11eb-bdf1-0242ac120034",
-    link2: "http://www.visitopatija.net/hr/gastronomija/kvarnerski-skamp",
-    link3: "https://www.spar.hr/dobar-tek/recepti/predjela/kamenicerockefeller",
-  },
-]
 
 export default {
   name: "Brut",
@@ -43,6 +25,34 @@ export default {
       winecard,
     }
   },
+   async mounted() {
+    fetch("http://localhost:3330/brut")
+      .then((r) => {
+        return r.json();
+      })
+      .then((data) => {
+        console.log("Podaci s backenda, brut", data);
+
+        this.winecard = data.map((element) => {
+          return {
+            logo: element.logo,
+            ime: element.name,
+            slika: element.img,
+            boja: element.color,
+            temp: element.temp,
+            cijena: element.price,
+            opis: element.about,
+            posluzi: element.serve,
+            jelo1: element.meal1,
+            jelo2: element.meal2,
+            jelo3: element.meal3,
+            link1: element.link1,
+            link2: element.link2,
+            link3: element.link3
+          };
+        });
+      });
+   },
   components: {
     WineCard
   }
